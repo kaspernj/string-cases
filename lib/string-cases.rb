f508eabc06301a46bcc4e8740d83b67121012c35
kaspernj <k@spernj.org>
2016-01-06 12:42:15 +0100

    Fixed pluralize of "key" to "keys" instead of "keies". expect-syntax. Simplifications. Upgraded gems

--- lib/string-cases.rb
+++ lib/string-cases.rb
@@ -1,22 +1,22 @@
 class StringCases
   def self.snake_to_camel(str)
-    return str.to_s.split("_").map{|w| w.capitalize }.join("")
+    str.to_s.split("_").map(&:capitalize).join("")
   end
 
   def self.camel_to_snake(str)
-    return str.to_s.gsub(/(.)([A-Z])/,'\1_\2').downcase
+    str.to_s.gsub(/(.)([A-Z])/,'\1_\2').downcase
   end
 
   def self.pluralize(str)
     str = "#{str}"
 
-    if str.end_with?("y")
+    if str.match(/([^aeiouy]|qu)y$/i)
       str = str.gsub(/y\Z/, "ies")
     else
       str << "s"
     end
 
-    return str
+    str
   end
 
   def self.singularize(str)
@@ -28,6 +28,6 @@ class StringCases
       str = str.gsub(/s\Z/, "")
     end
 
-    return str
+    str
   end
 end
